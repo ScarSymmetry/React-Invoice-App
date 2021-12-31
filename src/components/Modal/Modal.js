8e1e9f4ff9dfe0b4bd934dce14bc3699070b5c23
ScarSymmetry <caparisonhgs@gmail.com>
2022-01-01 02:51:17 +0300

    add body scroll lock

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,21 +1,36 @@
-import { createPortal } from "react-dom";
-import styles from "./Modal.module.scss";
+import { createPortal } from 'react-dom';
+import styles from './Modal.module.scss';
+import {
+  disableBodyScroll,
+  enableBodyScroll,
+  clearAllBodyScrollLocks,
+} from 'body-scroll-lock';
+import { useRef, useEffect } from 'react';
 
-const rootModal = document.getElementById("modal");
+const rootModal = document.getElementById('modal');
 
 const Modal = ({ children, isOpen = false, opaque }) => {
-	if (!isOpen) return null;
+  const modalRef = useRef();
+  useEffect(() => {
+    if (isOpen) disableBodyScroll(document.body);
+    return () => {
+      clearAllBodyScrollLocks();
+    };
+  }, [isOpen]);
 
-	return createPortal(
-		<div
-			className={`${styles.modalWrapper} ${
-				opaque ? styles.backgroundOpaque : null
-			}`}
-		>
-			{children}
-		</div>,
-		rootModal
-	);
+  if (!isOpen) return null;
+
+  return createPortal(
+    <div
+      ref={modalRef}
+      className={`${styles.modalWrapper} ${
+        opaque ? styles.backgroundOpaque : null
+      }`}
+    >
+      {children}
+    </div>,
+    rootModal
+  );
 };
 
 export default Modal;
