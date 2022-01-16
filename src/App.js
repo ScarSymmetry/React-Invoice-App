eece03ee871cb6f61a93b956986fb96ab361cedb
ScarSymmetry <caparisonhgs@gmail.com>
2022-01-16 17:42:11 +0300

    refactoring css/scss variables for the theme switching

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
         <Router>
           <Header />
           <Main />
-
+          
           <Route path='/invoices/:id'>
             <UserDetailsPage />
           </Route>
