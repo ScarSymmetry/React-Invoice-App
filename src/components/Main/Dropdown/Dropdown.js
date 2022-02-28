b54cf53392698983a05826ff9c56c3e80f07630f
ScarSymmetry <caparisonhgs@gmail.com>
2022-02-28 19:21:04 +0300

    fix merge conflict2

--- src/components/Main/Dropdown/Dropdown.js
+++ src/components/Main/Dropdown/Dropdown.js
@@ -1,72 +1,3 @@
-<<<<<<< HEAD
-import { useState, useRef } from 'react';
-import useClickOutside from '../../hooks/useClickOutside';
-
-import styles from './Dropdown.module.scss';
-
-const Dropdown = ({ open, onClickOutside }) => {
-  const modalRef = useRef();
-
-  useClickOutside(modalRef, () => {
-    if (open) onClickOutside(false);
-  });
-
-  const [dropDownValues, setDropDownValues] = useState([
-    {
-      id: 0,
-      value: 'draft',
-      checked: false,
-    },
-    {
-      id: 1,
-      value: 'pending',
-      checked: false,
-    },
-    {
-      id: 2,
-      value: 'paid',
-      checked: false,
-    },
-  ]);
-
-  const handleChange = (index) => {
-    setDropDownValues(
-      dropDownValues.map((option) => {
-        if (index === option.id) {
-        //   dispatch({
-        //     type: 'FILTER_INVOICE',
-        //     status: option.checked ? null : option.value,
-        //   });
-          return { ...option, checked: !option.checked };
-        }
-
-        return { ...option, checked: false };
-      })
-    );
-  };
-
-  if (!open) return null;
-
-  return (
-    <div ref={modalRef} className={styles.popUpInputs}>
-      <div className={styles.checkboxWrapper}>
-        {dropDownValues.map((box, index) => (
-          <div className={styles.checkboxContainer} key={index}>
-            <input
-              type='checkbox'
-              name={box.value}
-              id={box.id}
-              checked={box.checked}
-              onChange={() => {
-                handleChange(index);
-              }}
-            />
-            <label htmlFor={box.id}>{box.value}</label>
-          </div>
-        ))}
-      </div>
-    </div>
-=======
 import { useRef, useContext } from 'react';
 import useClickOutside from '../../hooks/useClickOutside';
 
@@ -119,7 +50,6 @@ const Dropdown = ({ open, onClickOutside }) => {
         </motion.div>
       )}
     </AnimatePresence>
->>>>>>> master
   );
 };
 
